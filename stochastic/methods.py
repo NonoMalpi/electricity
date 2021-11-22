40d4f692930cd85698c32585ef01034f5bbec37a
NonoMalpi <antoniomalpicamorales@gmail.com>
2021-11-22 22:19:12 +0000

    refact: Change simulation method from sequential to parallel

--- stochastic/methods.py
+++ stochastic/methods.py
@@ -3,6 +3,8 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 
+from joblib import Parallel, delayed
+
 from stochastic.coefficients import Coefficient
 
 
@@ -45,27 +47,34 @@ class EulerMaruyama:
 
         return y
 
+    def simulate_path(self, x0: np.ndarray, x0_dim: int, x0_time_step: int) -> np.ndarray:
+        y = self._set_y_array(x0=x0, x0_dim=x0_dim, x0_time_step=x0_time_step)
+        for t in range(x0_time_step, self.periods):
+            y_t_1 = y[:, t - 1]
+            y_t = y_t_1 + self.drift.get_value(x=y, t=t) * self.delta_t + \
+                  (1 / 2) * np.dot(self.diffusion.get_value(x=y, t=t),
+                                   np.random.normal(loc=self.nu, scale=self.sqrt_delta_t, size=(x0_dim, 1))
+                                   ).reshape(-1)
+            y_t = np.where(y_t < 0, 0, y_t)
+            y[:, t] = y_t
+        return y.flatten("F")
+
     def simulate(self, x0: np.ndarray, random_seed: float = 0) -> pd.DataFrame:
         """
 
         x0: np.ndarray (n_dim, ) or (n_dim, t)
             Initial value with (n_dim,) for one single timestep or (n_dim, t) for multiple timesteps
         """
-        sim_df = pd.DataFrame()
 
         np.random.seed(seed=random_seed)
         x0, x0_dim, x0_time_step = self._check_x0_input(x0=x0)
 
-        for i in range(self.num_sim):
-            y = self._set_y_array(x0=x0, x0_dim=x0_dim, x0_time_step=x0_time_step)
-            for t in range(x0_time_step, self.periods):
-                y_t_1 = y[:, t-1]
-                y_t = y_t_1 + self.drift.get_value(x=y, t=t)*self.delta_t + \
-                      (1/2) * np.dot(self.diffusion.get_value(x=y, t=t),
-                                     np.random.normal(loc=self.nu, scale=self.sqrt_delta_t, size=(x0_dim, 1))
-                                     ).reshape(-1)
-                y_t = np.where(y_t < 0, 0, y_t)
-                y[:, t] = y_t
-            sim_df[i] = y.flatten("F")
+        paths = Parallel(n_jobs=-1, verbose=0)(
+            delayed(self.simulate_path)(x0=x0, x0_dim=x0_dim, x0_time_step=x0_time_step)
+            for _ in range(self.num_sim)
+        )
+
+        sim_df = pd.DataFrame(paths).T
 
         return sim_df
+
