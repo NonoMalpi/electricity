2d67695f06b339aff614c9a953dee4e3c453d78c
NonoMalpi <antoniomalpicamorales@gmail.com>
2022-04-06 12:47:47 +0100

    :memo: Create PotencialaBase docstring

--- potenciala/potenciala.py
+++ potenciala/potenciala.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Dict, List, NoReturn
+from typing import Dict, List, NoReturn, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,17 +24,77 @@ class FigureShape(Enum):
 
 
 class PotencialaBase:
+    """ Basic operations for electricity price time series.
+
+    This class sets as a base to compute statistics of the electricity price time series as a function of the
+    electricity price value, which is called the independent variable (x), e.g, the  1 hour or 24 hour drift
+    as a function of the price value. It also performs transformations on the signal and the independent variable.
+
+    Attributes
+    ----------
+    signal_transformer: potenciala.transformers.Transformer
+        Class containing methods to apply transformation to the studied signal.
+
+    x_transformer: Transformer
+        Class containing methods to apply transformation to the independent variable.
+
+    signal_name: str
+        The transformed name of the studied signal.
+
+    metric_lag_time: List[int]
+        Number of time steps to compute statistics over the signal.
+
+    bin_size: int
+        Resolution of the independent variable.
+
+    x_col_name: str
+        Name of the independent variable.
+
+    df: pd.DataFrame
+        Clean dataframe with hourly records about the signal and the independent variable.
+    """
 
     def __init__(self,
                  df: pd.DataFrame,
                  signal_name: str,
                  metric_lag_time: List[int],
-                 bucket_method=BucketMethod.Cut,
-                 bin_size=2,
+                 bucket_method: BucketMethod = BucketMethod.Cut,
+                 bin_size: int = 2,
                  time_change: bool = True,
                  x_col_name: str = "x_label",
                  signal_transformation: str = None,
                  x_transformation: str = None):
+        """ Initialisation of the class.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            The raw dataframe containing electricity time series information.
+
+        signal_name: str
+            The raw name of the signal to study.
+
+        metric_lag_time: List[int]
+             Number of time steps to compute statistics over the signal.
+
+        bucket_method: BucketMethod
+            Method to discretise the independent variable, default = BucketMethod.Cut.
+
+        bin_size: int
+            Length between marks for the BucketMethod.Cut, default = 2.
+
+        time_change: bool
+            Amend the clock change issue for the time series, default = True.
+
+        x_col_name: str
+            Name of the independent variable, default = "x_label".
+
+        signal_transformation: str
+            Name of the Transformer to apply to the signal, default = None.
+
+        x_transformation: str
+            Name of the Transformer to apply to the independent variable, default = None.
+        """
 
         self.signal_transformer = TransformerFactory.build(transformer_type=signal_transformation)
         self.x_transformer = TransformerFactory.build(transformer_type=x_transformation)
@@ -47,13 +107,46 @@ class PotencialaBase:
         self.df = self._preprocess_input_df(df=df, time_change=time_change)
         self._bucketise_x(method=bucket_method)
 
-    def _transform_signal(self, df: pd.DataFrame, signal_name: str):
+    def _transform_signal(self, df: pd.DataFrame, signal_name: str) -> Tuple[str, pd.DataFrame]:
+        """ Apply signal transformation and renaming through signal_transformer attribute.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            The raw dataframe containing the electricity signal.
+
+        signal_name: str
+            The raw name of the signal to study.
+
+        Returns
+        -------
+        new_name: str
+            The signal new name after tranformation.
+
+        df: pd.DataFrame
+            The dataframe containing the transformed electricity signal.
+        """
         df = df.copy(deep=True)
         new_name = self.signal_transformer.rename_signal(signal_name=signal_name)
         df[new_name] = self.signal_transformer.transform(series=df[signal_name])
         return new_name, df
 
     def _preprocess_input_df(self, df: pd.DataFrame, time_change: bool) -> pd.DataFrame:
+        """ Clean dataframe if clock change is considered.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            The raw dataframe containing electricity time series information.
+
+        time_change: bool
+            Amend the clock change issue for the time series.
+
+        Returns
+        -------
+        result_df: pd.DataFrame
+            The dataframe with the clock change amendment.
+        """
 
         if not time_change:
             return df.copy(deep=True)
@@ -74,6 +167,13 @@ class PotencialaBase:
             return result_df
 
     def _bucketise_x(self, method: BucketMethod) -> NoReturn:
+        """ Transform independent variable through x_transfomer attribute and discretise it according to chosen method.
+
+        Parameters
+        ----------
+        method: BucketMethod
+            Method to discretise the independent variable.
+        """
 
         x_transformed = self.x_transformer.transform(self.df[self.signal_name])
 
@@ -95,6 +195,7 @@ class PotencialaBase:
             self.df[self.x_col_name] = x_transformed
 
     def _compute_drift(self) -> NoReturn:
+        """ Calculate signal drift for the different values of metric_lag_time attribute. """
         self.drift_cols = []
         for i in self.metric_lag_time:
             drift_col_name = f"drift_{i}"
@@ -102,6 +203,16 @@ class PotencialaBase:
             self.drift_cols.append(drift_col_name)
 
     def _plot_ts(self, ts: pd.DataFrame, signal_name: str) -> NoReturn:
+        """ Auxiliary function to plot the time series at hour level.
+
+        Parameters
+        ----------
+        ts: pd.Dataframe
+            The dataframe containing the signal time series to plot at hour level.
+
+        signal_name: str
+            The signal column name to plot.
+        """
         ts_index = (pd.to_datetime(ts["date"]) + pd.to_timedelta(ts["hour"] - 1, unit="H"))
         ts_df = pd.DataFrame(index=ts_index, data=ts[signal_name].values)
 
@@ -109,7 +220,18 @@ class PotencialaBase:
         ts_df.plot(ax=ax)
         ax.get_legend().remove()
 
-    def plot_ts(self, signal_name: str = None, period_filter: str = None):
+    def plot_ts(self, signal_name: str = None, period_filter: str = None) -> NoReturn:
+        """ Plot a time series of a signal over a desired period, by default it plots the time series of the
+        signal_name attribute.
+
+        Parameters
+        ----------
+        signal_name: str
+            The signal column name to plot, default = None.
+
+        period_filter: str
+            A pandas style query to filter time in the dataframe.
+        """
         if not signal_name:
             signal_name = self.signal_name
         self._plot_ts(self.df if not period_filter else self.df.query(period_filter), signal_name=signal_name)
