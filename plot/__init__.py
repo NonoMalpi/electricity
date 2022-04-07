94e6cd08a987fc4869430ff9e69d650eeb6ff230
NonoMalpi <antoniomalpicamorales@gmail.com>
2022-04-07 11:31:18 +0100

    :hammer: Add kernel plot auxiliary functions

--- plot/__init__.py
+++ plot/__init__.py
@@ -1,2 +1,3 @@
 from .colors import ImperialColors, MatplotlibDefaultColors, get_continuous_cmap
+from .kernel import plot_samples_kernel_mean_expected, plot_potential
 from .simulations import plot_simulation, plot_simulation_comparison
---
+++ plot/kernel.py
@@ -0,0 +1,66 @@
+from typing import NoReturn
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+from ml import GaussianKernel
+from plot import ImperialColors
+
+
+def plot_samples_kernel_mean_expected(ax: plt.Axes,
+                                      gk: GaussianKernel,
+                                      historical_mean: pd.Series,
+                                      ymax_lim: float = None,
+                                      lw: float = 1) -> NoReturn:
+    """ Plot simultaneously the observations, the kernel density, the expected value function from the kernel
+    and the frequentist mean.
+
+    Parameters
+    ----------
+    ax: plt.Axes
+        The axis to plot.
+
+    gk: GaussianKernel
+        The fitted kernel contained the samples, the grid and the sampled probability density function.
+
+    historical_mean: pd.Series
+        A series containing the frequentist mean of the samples used for the fitted kernel.
+
+    ymax_lim: float
+        Maximum y tick to represent, default = None.
+
+    lw: float
+        Line width for the representation of the historical mean and the expected value from the kernel, default = 1.
+    """
+
+    ax.plot(gk.samples[0], gk.samples[1], 'k.', markersize=1, alpha=0.2)
+    historical_mean.plot(ax=ax, lw=lw, color=ImperialColors.red.value)
+    ax.plot(gk.expected_value_function[0, :], gk.expected_value_function[1, :], lw=lw,
+            color=ImperialColors.blue.value)
+    cs = ax.contourf(gk.grid[0], gk.grid[1], gk.p,
+                     levels=100, cmap=plt.cm.gist_earth_r, alpha=0.5, antialiased=True)
+    if ymax_lim:
+        ax.set_ylim(0, ymax_lim)
+
+
+def plot_potential(ax: plt.axis, gk: GaussianKernel, historical_mean: pd.Series) -> NoReturn:
+    """ Plot potential coming from the fitted kernel and the frequentist mean.
+
+    Parameters
+    ----------
+    ax: plt.Axes
+        The axis to plot.
+
+    gk: GaussianKernel
+        The fitted kernel contained the samples, the grid and the sampled probability density function.
+
+    historical_mean: pd.Series
+        A series containing the frequentist mean of the samples used for the fitted kernel.
+    """
+    dx = gk.expected_value_function[0, 1] - gk.expected_value_function[0, 0]
+    ax.plot(gk.expected_value_function[0, :], (-1) * np.cumsum(gk.expected_value_function[1, :] * dx),
+            color=ImperialColors.blue.value)
+    ax.plot(historical_mean.index, (-1) * np.cumsum(historical_mean.values),
+            color=ImperialColors.red.value)
